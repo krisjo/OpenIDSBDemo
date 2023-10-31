b9520bcac8ba4a4e5157d2bbf28465778d01308b
krisjo <kristjan@dbvis.com>
2023-10-31 20:02:50 +0100

    Dump all claims from ID token on browser

--- two-providers/src/main/java/se/webstep/oauthdemo/OauthDemo.java
+++ two-providers/src/main/java/se/webstep/oauthdemo/OauthDemo.java
@@ -15,9 +15,6 @@
  */
 package se.webstep.oauthdemo;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
@@ -32,45 +29,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 @RestController
 public class OauthDemo extends WebSecurityConfigurerAdapter {
 
-	@RequestMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Map.of("name", principal.getAttribute("name"),
-				"email", principal.getAttribute("email"));
-	}
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Map.of("name", principal.getAttribute("name"),
+                "email", principal.getAttribute("email"),
+                "iss", principal.getAttribute("iss"));
+    }
 
-	@GetMapping("/oidc-principal")
-	public Map<String, Object> getOidcUserPrincipal(
-			@AuthenticationPrincipal OidcUser principal) {
-		return Collections.singletonMap("principal", principal.getClaims().toString());
-	}
+    @GetMapping("/oidc-principal")
+    public String getOidcUserPrincipal(@AuthenticationPrincipal OidcUser principal) {
+        Map<String, Object> claims = principal.getClaims();
+        return claims.keySet().stream().map(k -> k + " = " + claims.get(k).toString()).collect(Collectors.joining("<br/>"));
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http
-			.authorizeRequests(a -> a
-				.antMatchers("/", "/error", "/webjars/**").permitAll()
-				.anyRequest().authenticated()
-			)
-			.exceptionHandling(e -> e
-				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-			)
-			.csrf(c -> c
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-			)
-			.logout(l -> l
-				.logoutSuccessUrl("/").permitAll()
-			)
-			.oauth2Login();
-		// @formatter:on
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .oauth2Login();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(OauthDemo.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OauthDemo.class, args);
+    }
 
 }
